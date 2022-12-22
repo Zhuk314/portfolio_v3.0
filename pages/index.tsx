51b17ecfc2b7992f2ee6307142c4f409afdd1fc1
Yurii Zhuk <iamyuriizhuk@gmail.com>
2022-12-21 18:29:37 -0800

    Added Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from "next";
+import Header from "../components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <>
+    <div>
       <Head>
-        <title>Yurii Portfolio</title>
+        <title>Create Next App</title>
       </Head>
-      <h1>My portfolio!</h1>
-    </>
+      <Header />
+    </div>
   );
-}
+};
+
+export default Home;
