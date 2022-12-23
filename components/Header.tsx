865624ebb91300d55e6bcc831d807a5eb6d95aac
Yurii Zhuk <iamyuriizhuk@gmail.com>
2022-12-23 12:40:32 -0800

    Made responsive navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,24 +4,26 @@ type HeaderProps = {};
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-col justify-center md:flex-row md:justify-between">
       <div>
-        <p className="text-white text-5xl">
+        <p className="hidden md:inline-flex text-white text-5xl">
           <a href="#">Ж</a>
         </p>
       </div>
-      <div className="flex flex-row justify-evently">
-        <p className="text-white text-2xl p-2 font-light">
+
+      <div className="flex flex-col justify-center md:flex-row md:justify-between md:w-80">
+        <p className="text-white flex justify-center text-2xl p-2 font-light">
           <a href="#">About</a>
         </p>
-        <p className="text-white text-2xl p-2 font-light">
+        <p className="text-white flex justify-center text-2xl p-2 font-light">
           <a href="#">Skills</a>
         </p>
-        <p className="text-white text-2xl p-2 font-light">
+        <p className="text-white flex justify-center text-2xl p-2 font-light">
           <a href="#">Experience</a>
         </p>
       </div>
-      <div>
+
+      <div className="flex justify-center">
         <SocialIcon
           url="https://www.linkedin.com/in/yurii-zhuk/"
           fgColor="white"
@@ -32,12 +34,14 @@ export const Header: React.FC<HeaderProps> = ({}) => {
           fgColor="white"
           bgColor="transparent"
         />
-        <SocialIcon
-          className="cursor-ponter"
-          network="email"
-          fgColor="white"
-          bgColor="transparent"
-        />
+        <div className="cursor-pointer text-white">
+          <SocialIcon
+            className="cursor-ponter"
+            network="email"
+            fgColor="white"
+            bgColor="transparent"
+          />
+        </div>
       </div>
     </div>
   );
