2868fb6ea22d02ae5460e680e78bc77088e446a2
Yurii Zhuk <iamyuriizhuk@gmail.com>
2022-12-26 18:56:10 -0800

    Animation and speed adjusments

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,34 +1,48 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import Image from "next/image";
-import profilePic from "../public/hero.jpg";
+import { motion } from "framer-motion";
 
 export const Welcome: React.FC = () => {
-  const [text, count] = useTypewriter({
+  const [text] = useTypewriter({
     words: [
       "Welcome to my portfolio!",
       "How Are You Today?",
       "I am Yurii Zhuk.",
       "Would You Like Some Coffee?",
+      "Eat, Sleep, Code, Repeat!",
     ],
     loop: true,
-    delaySpeed: 2500,
+    delaySpeed: 3500,
+    deleteSpeed: 35,
   });
   return (
     <section className="h-screen flex flex-col item-center justify-center bg-backgroundPrimary snap-center">
-      <Image
-        className="relative rounded-full mx-auto object-cover"
-        src="/hero.jpg"
-        alt="profileImage"
-        width={225}
-        height={225}
-      />
-      <div className="relative mx-auto text-4xl md:text-6xl text-textPrimary text-center lg:tracking-wider">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9, y: -50 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <Image
+          className="relative rounded-full mx-auto object-cover"
+          src="/hero.jpg"
+          alt="profileImage"
+          width={225}
+          height={225}
+        />
+      </motion.div>
+
+      <motion.div
+        className="relative mx-auto text-4xl md:text-6xl text-textPrimary text-center lg:tracking-wider"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 3, delay: 2 }}
+      >
         <h1>
           <span>{text}</span>
           <Cursor />
         </h1>
-      </div>
+      </motion.div>
     </section>
   );
 };
