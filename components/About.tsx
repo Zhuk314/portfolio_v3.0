6dbeca9166948f2e036a8168297fa5cd75e1c943
Yurii Zhuk <iamyuriizhuk@gmail.com>
2022-12-27 23:40:26 -0800

    Made correct alignment

--- components/About.tsx
+++ components/About.tsx
@@ -3,16 +3,15 @@ import Image from "next/image";
 
 export const About: React.FC = ({}) => {
   return (
-    <div className="h-screen space-y-8 ">
-      <div className="flex justify-center">
-        <h3 className="text-3xl tracking-[15px] uppercase md:text-4xl text-textPrimary">
-          About
-        </h3>
-      </div>
-      <div className="flex flex-col items-center w-11/12 m-auto md:space-x-5 xl:space-x-20 md:items-start md:flex-row max-w-7xl">
-        <div className="md:w-1/2">
+    <div className="flex flex-col h-screen mx-auto space-y-8 text-center">
+      <h3 className="text-3xl tracking-[15px] uppercase md:text-4xl text-textPrimary">
+        About
+      </h3>
+
+      <div className="flex flex-col items-center m-auto space-y-5 md:w-11/12 md:space-y-0 md:space-x-5 xl:space-x-20 md:items-start md:flex-row max-w-7xl">
+        <div className="flex justify-center md:w-1/2">
           <Image
-            className="rounded-3xl w-[500px]"
+            className="w-80 rounded-full md:rounded-3xl lg:w-[400px] xl:w-[500px] xl:rounded-xl"
             src={"/hero_about.jpg"}
             alt={"About Profile Image"}
             width={700}
