b4d81da096ece0d19534ebdcaf2da6b96b76b689
Yurii Zhuk <iamyuriizhuk@gmail.com>
2022-12-28 16:57:36 -0800

    Added snap for now

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Skills: React.FC = () => {
   return (
-    <div className="text-center ">
+    <div className="h-screen text-center">
       <h3 className="title">Skills</h3>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,20 @@ import { Header, Welcome, About, Skills } from "../components";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-backgroundPrimary">
+    <div className="h-screen overflow-y-scroll bg-backgroundPrimary snap-y snap-mandatory">
       <Head>
         <title>Yurii Zhuk</title>
       </Head>
 
-      <section id="home">
+      <section id="home" className="snap-start ">
         <Welcome />
       </section>
 
-      <section id="about">
+      <section id="about" className="snap-center">
         <About />
       </section>
 
-      <section id="skills">
+      <section id="skills" className="snap-center">
         <Skills />
       </section>
 
