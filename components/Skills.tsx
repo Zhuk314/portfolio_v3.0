d753af592f38afb6761d90a50afd576d7df9bd2c
Yurii Zhuk <iamyuriizhuk@gmail.com>
2022-12-29 17:22:31 -0800

    Installed styled icon and used icon for skill

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,17 +3,17 @@ import { Skill } from "./Skill";
 
 export const Skills: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen space-y-8 text-center">
+    <div className="flex flex-col h-screen m-auto space-y-8 text-center max-w-7xl">
       <h3 className="title">Skills</h3>
 
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-        <Skill name="defaultSkill" />
-        <Skill name="defaultSkill" />
-        <Skill name="defaultSkill" />
-        <Skill name="defaultSkill" />
-        <Skill name="defaultSkill" />
-        <Skill name="defaultSkill" />
-        <Skill name="defaultSkill" />
+        <Skill name="defaultSkill1" />
+        <Skill name="defaultSkill2" />
+        <Skill name="defaultSkill3" />
+        <Skill name="defaultSkill4" />
+        <Skill name="defaultSkill5" />
+        <Skill name="defaultSkill6" />
+        <Skill name="defaultSkill7" />
       </div>
     </div>
   );
