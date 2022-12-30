54273a0b4f3360e7418b5bb57346a0b5f207c987
Zhuk314 <78105676+Zhuk314@users.noreply.github.com>
2022-12-29 17:23:45 -0800

    Installed styled icon and used icon for skill (#3)

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BadgeCheck } from "@styled-icons/boxicons-solid/BadgeCheck";
 
 type SkillProps = {
   name: string;
@@ -8,11 +9,12 @@ type SkillProps = {
 export const Skill: React.FC<SkillProps> = ({ name, isLeft, ...props }) => {
   return (
     <div
-      className="m-auto text-xl w-96 text-textPrimary bg-gradient-to-r 
+      className=" relative m-auto text-xl max-w-96 text-textPrimary bg-gradient-to-r 
       from-[#092143] to-[#FE4D5B] rounded-full p-2"
       {...props}
     >
-      {name}
+      <BadgeCheck className="w-8 fill-[#30CC8E]" />
+      <span className="mx-20">{name}</span>
     </div>
   );
 };
