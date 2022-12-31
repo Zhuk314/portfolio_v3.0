7cbb0d618de5eff1369467fda31989a4433aa31c
Yurii Z <iamyuriizhuk@gmail.com>
2022-12-31 01:01:15 -0800

    Added tile and tile group, got infinite loop and error in the console

--- components/SkillsTiles/Tile.tsx
+++ components/SkillsTiles/Tile.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-export const Tile: React.FC = () => {
-  return <div className=" rounded-xl bg-white w-full h-full">Tile</div>;
+export const Tile: React.FC<{ gArea: string }> = ({ gArea }) => {
+  //className="rounded-xl bg-white w-full h-full"
+  return (
+    <TileContainer gArea={gArea}>
+      <div>Tile</div>
+    </TileContainer>
+  );
 };
+
+const TileContainer = styled.div<{ gArea: string }>`
+  grid-area: ${({ gArea }) => gArea}; ;
+`;
--- components/SkillsTiles/TilesGroup.tsx
+++ components/SkillsTiles/TilesGroup.tsx
@@ -1,40 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tile } from "./Tile";
+import styled from "styled-components";
 
-export const TilesGroup: React.FC = () => {
+interface TilesGroupProps {
+  skills?: string[];
+}
+
+export const TilesGroup: React.FC<TilesGroupProps> = ({
+  skills = ["JS", "Java"],
+}) => {
+  const [letter, setLetter] = useState("a");
+
+  const getGArea = (char: string) => {
+    //todo: add turnback on v+ letter
+    const nextLetter =
+      char.substring(0, char.length - 1) +
+      String.fromCharCode(char.charCodeAt(char.length - 1) + 1);
+    //setLetter(nextLetter);
+    console.log("currentChar " + char);
+    console.log("nextLetter " + nextLetter);
+    return char;
+  };
+
   return (
-    <>
-      <div className=" grid grid-cols-4 gap-2">
-        <div className="col-span-1">
-          <Tile />
-        </div>
-        <div>
-          <Tile />
-        </div>
-        <div>
-          <Tile />
-        </div>
-        <div className="row-span-2">
-          <Tile />
-        </div>
-
-        <div className="col-span-2 row-span-2">
-          <Tile />
-        </div>
-        <div>
-          <Tile />
-        </div>
-        <div>
-          <Tile />
-        </div>
-        <div>
-          <Tile />
-        </div>
-
-        <div>
-          <Tile />
-        </div>
-      </div>
-    </>
+    <MainContainer>
+      {skills.map((skill, count) => {
+        console.log("count " + count);
+        return <Tile gArea={"a"} key={"skill_" + count} />;
+      })}
+    </MainContainer>
   );
 };
+
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-areas:
+    "a b c d"
+    "e e f d"
+    "e e g h"
+    "i j k k"
+    "l m m n"
+    "o m m p"
+    "q r s t"
+    "q u v v";
+  gap: 10px;
+  padding: 10px;
+`;
